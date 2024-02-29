39dd7b96206559a058da4614ef31cfc3460deb8a
Dalbizurez <dalbizurez@correo.url.edu.gt>
2024-02-29 08:54:37 -0600

    Update lista.py
    Clase Lista con el metodo ingresar al principio

--- lista.py
+++ lista.py
@@ -4,3 +4,14 @@ class Node:
         self.next = None
     def __str__(self) -> str:
         return str(self.data)
+    
+class LinkedList:
+    def __init__(self) -> None:
+        self.head : Node = None
+        self.length = 0
+    
+    def insert_head(self, data):
+            old = self.head
+            self.head = Node(data)
+            self.head.next = old
+            self.length += 1
